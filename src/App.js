6e9b0d844a31c1581bba63f35087c1f26db6596f
Giriraj-Roy <girirajroy2001@gmail.com>
2022-10-06 10:41:43 +0530

    header is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header style={{'position':'fixed'}}>Header</header>
+    )
+}
+
+export default Header
