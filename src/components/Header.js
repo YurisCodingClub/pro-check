614d39c57fed1a7661a7a7af693171a5cb0fbeff
Giriraj-Roy <girirajroy2001@gmail.com>
2022-10-12 00:17:01 +0530

    header and logo

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='logo'>
+                <img src='./logo.jpg' alt='logo'/>
+            </div>
+        </header>
+    )
+}
+
+export default Header
