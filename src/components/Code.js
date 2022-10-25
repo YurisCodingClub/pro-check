a728e86b870525917a941b030bb496b5b2f37407
Ezenwekwe Chidera <marvinezenwekwe@gmail.com>
2022-10-25 23:38:55 +0100

    Overwrite in Code.js component

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,17 +1,18 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment,useEffect, useState } from "react"
+import { myTasks } from "../assets/constants";
+
 
 const Code = () => {
   const [check, setCheck] = useState(false);
-  const[task, setTask] = useState({});
   const [checkList, setCheckList] = useState([]);
 
 
   //Function to check if localStorage has checklist data
   const fetchList = () => {
-    const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    const savedTasks = JSON.parse(localStorage.getItem('checklist'));
 
     if (savedTasks === null || savedTasks.length === 0) {
-      localStorage.setItem('tasks', JSON.stringify([{done: check, item: 'Create React App'}]));
+      localStorage.setItem('checklist', JSON.stringify(myTasks));
     }
 
     else {
@@ -23,61 +24,39 @@ const Code = () => {
   //Fetching checklist on first load
   useEffect(() => {
     fetchList();
-  }, []);
-
-  //Function to add new tasks to checkList
-  function addNewTask() {
-    localStorage.setItem('tasks', JSON.stringify([...checkList, task]));
-    fetchList();
-  }
+  }, [check]);
 
   //Function to check/uncheck items 
-  function checkItem(task) {
-    if (task.done === true) {
-      setCheck(task.done = false)
-      const changedTask = checkList.find(obj => obj.item === task.item);
-      Object.assign(changedTask, task);
-      localStorage.setItem('tasks', JSON.stringify(checkList));
+  function checkItem(item) {
+    if (item.checked === true) {
+      setCheck(item.checked = false)
+      const changedTask = checkList.find(obj => obj.task === item.task);
+      Object.assign(changedTask, item);
+      localStorage.setItem('checklist', JSON.stringify(checkList));
     }
 
     else {
-      setCheck(task.done = true)
-      const changedTask = checkList.find(obj => obj.item === task.item);
-      Object.assign(changedTask, task);
-      localStorage.setItem('tasks', JSON.stringify(checkList))
+      setCheck(item.checked = true)
+      const changedTask = checkList.find(obj => obj.task === item.task);
+      Object.assign(changedTask, item);
+      localStorage.setItem('checklist', JSON.stringify(checkList))
     }
   }
 
 
   return (  
     <Fragment>
-      <h1 style={{'textAlign': 'center'}}> 
-        Project Checklist 
-      </h1>
-
-      <form action="">
-        <input 
-          type="text" 
-          placeholder="Insert Task..." 
-          style={{'marginRight': '10px'}} 
-          onChange={(e) => setTask({done: check, item: e.target.value})}
-        />
-        <input type="button" value="Add New Task" onClick={addNewTask} />
-      </form>
-
+      <h1> Code and Live View </h1>
+      <hr />
       <div style={{'marginTop': '20px'}}>
-        {checkList.map((task, index) => (
-          <label 
-            key={index} 
-            style={{'display': 'block'}}
-            onClick={() => {checkItem(task)}}
-          >
+        {checkList.map((item, index) => (
+          <label key={index}>
             <input 
               type="checkbox" 
-              checked={task.done}
-              onChange={() => {console.log("Value change!")}}
-             />
-            {task.item}
+              checked={item.checked === 'false' ? false : true} 
+              onChange={() => checkItem(item)}
+            />
+            <div style={{'display': 'inline'}}  dangerouslySetInnerHTML={{__html: item.task}}></div>
           </label>
         ))}
       </div>
