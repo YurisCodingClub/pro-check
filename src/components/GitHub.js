1232bf134ff3d62f057d64d61df7c92bda87bc61
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-11-06 18:23:01 +0600

    add localstorage functionality and refactor code

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,37 +1,99 @@
-import { githubRepoTasks as tasks } from "../assets/constants";
 import { useState, useEffect } from "react";
 
 const GitHub = () => {
-    const [taskNames, setTaskNames] = useState(
-        JSON.parse(localStorage.getItem("githubTasksStored"))
-    );
+    const githubItems = [
+        {
+            id: 1,
+            checked: false,
+            item: "The repository has a README with all the information about the project",
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "The repo has a link to the live view in the about section",
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "The repo has a short introduction in the <strong> about section",
+        },
+        {
+            id: 4,
+            checked: false,
+            item: "The repository has tags in the about section",
+        },
+        {
+            id: 5,
+            checked: false,
+            item: "Open issues are created and labeled",
+        },
+        {
+            id: 6,
+            checked: false,
+            item: "A thumbnail is added to the social preview in GitHub settings",
+        },
+        {
+            id: 7,
+            checked: false,
+            item: "The repo is visible",
+        },
+        {
+            id: 8,
+            checked: false,
+            item: "Merged/unused branches are deleted",
+        },
+    ];
+
+    const [items, setItems] = useState([]);
+
+    const fetchList = () => {
+        const githubItemsStored = JSON.parse(
+            localStorage.getItem("githubItemsStored")
+        );
+
+        if (githubItemsStored === null || githubItemsStored.length === 0) {
+            localStorage.setItem(
+                "githubItemsStored",
+                JSON.stringify(githubItems)
+            );
+        } else {
+            setItems(githubItemsStored);
+        }
+    };
 
     useEffect(() => {
-        localStorage.setItem("githubTasksStored", JSON.stringify(tasks));
-    }, [taskNames]);
+        fetchList();
+    }, [items]);
 
     const handleCheck = (id) => {
-        const listItems = tasks.map((task) =>
-            task.id === id ? { ...task, checked: !task.checked } : task
+        const listItems = items.map((item) =>
+            item.id === id ? { ...item, checked: !item.checked } : item
         );
-        setTaskNames(listItems);
+        setItems(listItems);
+        localStorage.setItem("githubItemsStored", JSON.stringify(listItems));
     };
 
     return (
-        <div>
-            <h2>GitHub Repositories</h2>
-
-            <ul>
-                {tasks.map((task) => (
-                    <li className="task">
+        <div style={{ padding: "40px 30px" }}>
+            <h2 style={{ fontSize: "32px" }}>GitHub Repositories</h2>
+            <hr />
+            <ul style={{ margin: "0", padding: "0" }}>
+                {items.map((item) => (
+                    <li
+                        style={{
+                            listStyle: "none",
+                            fontSize: "16px",
+                            lineHeight: "2",
+                        }}
+                        className="item"
+                        key={item.id}
+                    >
                         <input
-                            onChange={() => handleCheck(task.id)}
                             type="checkbox"
-                            checked={task.checked}
+                            onChange={() => handleCheck(item.id)}
+                            checked={item.checked}
                         />
-                        <label type="checkbox" checked={task.checked}>
-                            {task.task}
-                        </label>
+                        <label style={{ display: "inline" }}>{item.item}</label>
                     </li>
                 ))}
             </ul>
