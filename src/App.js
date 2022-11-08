bbb4abc5cfd1dcb4f3d348f0d524a976602051f6
Julia Undeutsch <JUndeutsch@gmx.at>
2022-11-08 19:46:51 +0100

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Header from "./components/Header";
-import CodeTasks from "./components/CodeTasks";
-import GitHubTasks from "./components/GitHubTasks";
+import Header from './components/Header';
+import CodeTasks from './components/CodeTasks';
+import GitHubTasks from './components/GitHubTasks';
 
 function App() {
   return (
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,79 +1,78 @@
-//Hard coed checklist
 export const codeTasks = [
   {
     id: 1,
     checked: false,
-    task: "The project is deployed with Netlify, Vercel, Heroku, or any other platform",
+    task: 'The project is deployed with Netlify, Vercel, Heroku, or any other platform',
   },
   {
     id: 2,
     checked: false,
-    task: "With Lighthouse in live view, performance, best practice, accessibility, and SEO are at least 90% (green)",
+    task: 'With Lighthouse in live view, performance, best practice, accessibility, and SEO are at least 90% (green)',
   },
   {
     id: 3,
     checked: false,
-    task: "The project is fully responsive (tested in different browsers and on different device views)",
+    task: 'The project is fully responsive (tested in different browsers and on different device views)',
   },
   {
     id: 4,
     checked: false,
-    task: "When using Accessibility Insights (Google Chrome), the deployed version passed the automatic checks and review",
+    task: 'When using Accessibility Insights (Google Chrome), the deployed version passed the automatic checks and review',
   },
   {
     id: 5,
     checked: false,
-    task: "All the links in the code are working and leading in the right direction",
+    task: 'All the links in the code are working and leading in the right direction',
   },
   {
     id: 6,
     checked: false,
-    task: "The code has a good folder structure and follows best practices (folder names small, components large, utils functions small, etc)",
+    task: 'The code has a good folder structure and follows best practices (folder names small, components large, utils functions small, etc)',
   },
   {
     id: 7,
     checked: false,
-    task: "Code is refactored, clean (consistency on tabs, single/double quotes, etc) and as short as possible (e.g. margin-top: 10px; margin-right: 25px; → margin: 10px 25px 0 0;)",
+    task: 'Code is refactored, clean (consistency on tabs, single/double quotes, etc) and as short as possible (e.g. margin-top: 10px; margin-right: 25px; → margin: 10px 25px 0 0;)',
   },
   {
     id: 8,
     checked: false,
-    task: "The package.json does not contain any unused packages",
+    task: 'The package.json does not contain any unused packages',
   },
   {
     id: 9,
     checked: false,
-    task: "The code is free of unused variables, functions, console.log, comments, etc.",
+    task: 'The code is free of unused variables, functions, console.log, comments, etc.',
   },
   {
     id: 10,
     checked: false,
-    task: "The project is free of unused components, files, etc. which are automatically created when using Create-React-App",
+    task: 'The project is free of unused components, files, etc. which are automatically created when using Create-React-App',
   },
   {
     id: 11,
     checked: false,
-    task: "The project has a unique icon, logos in different sizes (manifest), and a thumbnail",
+    task: 'The project has a unique icon, logos in different sizes (manifest), and a thumbnail',
   },
   {
     id: 12,
     checked: false,
-    task: "The project has a customized title (in HTML <title>My App</title> tag) and a favicon",
+    task: 'The project has a customized title (in HTML <title>My App</title> tag) and a favicon',
   },
   {
     id: 13,
     checked: false,
-    task: "To meet responsiveness, you have preferred CSS Calculation over @media queries to make the code cleaner, if possible",
+    task: 'To meet responsiveness, you have preferred CSS Calculation over @media queries to make the code cleaner, if possible',
   },
   {
     id: 14,
     checked: false,
-    task: "In case of many file imports, use named export (all files of a folder in one index.js file)",
+    task: 'In case of many file imports, use named export (all files of a folder in one index.js file)',
   },
   {
     id: 15,
     checked: false,
-    task: "The project uses the latest versions and methodologies",
+    task: 'The project uses the latest versions and methodologies',
   },
 ];
 
@@ -81,41 +80,41 @@ export const githubTasks = [
   {
     id: 51,
     checked: false,
-    task: "The repository has a README.md with all the information about the project",
+    task: 'The repository has a README.md with all the information about the project',
   },
   {
     id: 52,
     checked: false,
-    task: "The repository has a link to the live view in the about section",
+    task: 'The repository has a link to the live view in the about section',
   },
   {
     id: 53,
     checked: false,
-    task: "The repository has a short introduction in the about section",
+    task: 'The repository has a short introduction in the about section',
   },
   {
     id: 54,
     checked: false,
-    task: "The repository has tags in the about section",
+    task: 'The repository has tags in the about section',
   },
   {
     id: 55,
     checked: false,
-    task: "Open issues are created and labeled",
+    task: 'Open issues are created and labeled',
   },
   {
     id: 56,
     checked: false,
-    task: "A thumbnail is added to the social preview in GitHub settings",
+    task: 'A thumbnail is added to the social preview in GitHub settings',
   },
   {
     id: 57,
     checked: false,
-    task: "The repository is visible",
+    task: 'The repository is visible',
   },
   {
     id: 58,
     checked: false,
-    task: "Merged/unused branches are deleted",
+    task: 'Merged/unused branches are deleted',
   },
 ];
